4f7835982f4c7cd12c57480aa79c5e0b15cc9355
Robert Moore <me@robdmoore.id.au>
2012-06-11 22:01:51 +0800

    Added relevant initial AutofacContrib.NSubstitute code from https://bitbucket.org/mdaviesnet/autofac (originally from https://bitbucket.org/mamadero/autofac)

---
+++ AutofacContrib.NSubstitute.Tests/AutoMockFixture.cs
@@ -0,0 +1,142 @@
+﻿using NSubstitute;
+using NSubstitute.Core;
+using NSubstitute.Exceptions;
+using NUnit.Framework;
+
+namespace AutofacContrib.NSubstitute.Tests
+{
+    [TestFixture]
+    public sealed class AutoMockFixture
+    {
+        #region stubs
+        public interface IServiceA
+        {
+            void RunA();
+        }
+
+        public interface IServiceB
+        {
+            void RunB();
+        }
+
+        public class ServiceA : IServiceA
+        {
+            public void RunA() { }
+        }
+
+        public sealed class TestComponent
+        {
+            private readonly IServiceA _serviceA;
+            private readonly IServiceB _serviceB;
+
+            public TestComponent(IServiceA serviceA, IServiceB serviceB)
+            {
+                this._serviceA = serviceA;
+                this._serviceB = serviceB;
+            }
+
+            public void RunAll()
+            {
+                this._serviceA.RunA();
+                this._serviceB.RunB();
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// Defaults the constructor is loose.
+        /// </summary>
+        [Test]
+        public void DefaultConstructorIsLoose()
+        {
+            using (var mock = new AutoMock())
+            {
+                RunWithSingleSetupationTest(mock);
+            }
+        }
+
+        [Test]
+        public void ProvideMock()
+        {
+            using (var autoMock = new AutoMock())
+            {
+                var mockA = Substitute.For<IServiceA>();
+                autoMock.Provide(mockA);
+
+                var component = autoMock.Resolve<TestComponent>();
+                component.RunAll();
+
+                mockA.Received().RunA();
+            }
+        }
+
+        [Test]
+        public void ProvideImplementation()
+        {
+            using (var mock = new AutoMock())
+            {
+                var serviceA = mock.Provide<IServiceA, ServiceA>();
+
+                Assert.IsNotNull(serviceA);
+                Assert.IsFalse(serviceA is ICallRouter);
+            }
+        }
+
+        [Test]
+        public void DefaultConstructorWorksWithAllTests()
+        {
+            using (var mock = new AutoMock())
+            {
+                RunTest(mock);
+            }
+        }
+
+        [Test]
+        public void WorksWithUnmetSetupations()
+        {
+            using (var loose = new AutoMock())
+            {
+                RunWithSingleSetupationTest(loose);
+            }
+        }
+
+        [Test]
+        [ExpectedException(typeof(ReceivedCallsException))]
+        public void NormalSetupationsAreVerified()
+        {
+            using (var mock = new AutoMock())
+            {
+                SetUpSetupations(mock);
+            }
+        }
+
+        [Test]
+        public void ProperInitializationIsPerformed()
+        {
+            var autoMock = new AutoMock();
+            Assert.IsNotNull(autoMock.Container);
+        }
+
+        private static void RunTest(AutoMock mock)
+        {
+            var component = mock.Resolve<TestComponent>();
+            component.RunAll();
+
+            SetUpSetupations(mock);
+        }
+
+        private static void SetUpSetupations(AutoMock autoMock)
+        {
+            autoMock.Resolve<IServiceB>().Received().RunB();
+            autoMock.Resolve<IServiceA>().Received().RunA();
+        }
+
+        private static void RunWithSingleSetupationTest(AutoMock autoMock)
+        {
+            var component = autoMock.Resolve<TestComponent>();
+            component.RunAll();
+
+            autoMock.Resolve<IServiceB>().Received().RunB();
+        }
+    }
+}
---
+++ AutofacContrib.NSubstitute/AutoMock.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using Autofac;
+using Autofac.Builder;
+using Autofac.Core;
+using Autofac.Features.ResolveAnything;
+using IContainer = Autofac.IContainer;
+using SubstituteAlias = NSubstitute.Substitute;
+
+namespace AutofacContrib.NSubstitute
+{
+
+    /// <summary>
+    /// Auto mocking container using <see cref="Autofac"/> and <see cref="NSubstitute"/>.
+    /// </summary>
+    public class AutoMock : IDisposable
+    {
+        /// <summary>
+        /// <see cref="IContainer"/> that handles the component resolution.
+        /// </summary>
+        public IContainer Container { get; private set; }
+
+        /// <summary>
+        /// Create an AutoMock.
+        /// </summary>
+        public AutoMock()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterSource(new AnyConcreteTypeNotAlreadyRegisteredSource());
+            builder.RegisterSource(new NSubstituteRegistrationHandler());
+            Container = builder.Build();
+        }
+
+
+        /// <summary>
+        /// Verifies mocks and disposes internal container.
+        /// </summary>
+        public void Dispose()
+        {
+            Container.Dispose();
+        }
+
+        /// <summary>
+        /// Resolve the specified type in the container (register it if needed)
+        /// </summary>
+        /// <typeparam name="T">Service</typeparam>
+        /// <param name="parameters">Optional parameters</param>
+        /// <returns>The service.</returns>
+        public T Resolve<T>(params Parameter[] parameters)
+        {
+            return Container.Resolve<T>(parameters);
+        }
+
+        /// <summary>
+        /// Resolve the specified type in the container (register it if needed)
+        /// </summary>
+        /// <typeparam name="TService">Service</typeparam>
+        /// <typeparam name="TImplementation">The implementation of the service.</typeparam>
+        /// <param name="parameters">Optional parameters</param>
+        /// <returns>The service.</returns>
+        public TService Provide<TService, TImplementation>(params Parameter[] parameters)
+        {
+            Container.ComponentRegistry.Register(
+                RegistrationBuilder.ForType<TImplementation>().As<TService>().InstancePerLifetimeScope().CreateRegistration());
+
+            return Container.Resolve<TService>(parameters);
+        }
+
+        /// <summary>
+        /// Resolve the specified type in the container (register specified instance if needed)
+        /// </summary>
+        /// <typeparam name="TService">Service</typeparam>
+        /// <returns>The instance resolved from container.</returns>
+        public TService Provide<TService>(TService instance)
+            where TService : class
+        {
+            Container.ComponentRegistry.Register(
+                RegistrationBuilder.ForDelegate((c, p) => instance).InstancePerLifetimeScope().CreateRegistration());
+
+            return Container.Resolve<TService>();
+        }
+
+        /// <summary>
+        /// Crates and returns a substitute. This is useful mainly for concrete classes where NSubstitutes won't be created by default
+        /// For advanced uses consider using directly Substitute.For and then calling <see cref="Provide{TService}"/> so that type is used on dependencies for other Resolved types.
+        /// </summary>
+        /// <typeparam name="TService">Service</typeparam>
+        /// <returns>The instance resolved from container.</returns>
+        public TService SubstituteFor<TService>() where TService : class
+        {
+            // SubstituteAlias=Substitute, but I have to use an Alias since the class name conflics with this member and can't use FQN since it conflics with this namesace
+            var substitute = SubstituteAlias.For<TService>();
+            return Provide(substitute);
+        }
+    }
+}
