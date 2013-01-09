73b83259d79a41e093ee5fbb5e5f62ef9e8a2a13
Michael Whelan <mjmwdev@gmail.com>
2013-01-09 10:26:02 +0000

    Spiked an AMC for a concrete SUT
    and its constructor parameter. Not sure whether to limit Dependency<T>
    to constructor parameters and SubFor for other subs, or just have one
    SubFor for all subs....?

---
+++ AutofacContrib.NSubstitute/AutoSub.cs
@@ -0,0 +1,28 @@
+﻿namespace AutofacContrib.NSubstitute
+{
+    public class AutoSub<TSut> where TSut : class
+    {
+        private TSut _sut;
+        private AutoSubstitute _autoSubstitute = new AutoSubstitute();
+
+        public AutoSub()
+        {
+            _sut = _autoSubstitute.ResolveAndSubstituteFor<TSut>();
+        }
+
+        public TSut SUT()
+        {
+            return _sut;
+        }
+
+        public T Dependency<T>() where T : class
+        {
+            return _autoSubstitute.Resolve<T>();
+        }
+
+        public T SubFor<T>() where T : class
+        {
+            return _autoSubstitute.ResolveAndSubstituteFor<T>();
+        }
+    }
+}
